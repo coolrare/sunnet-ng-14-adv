a3059131e2e5d8b06ae434d1ec92afc73c2cebad
Will <doggy.huang@gmail.com>
2024-06-06 15:31:30 +0800

    示範抓路由參數的方法

--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './tables.component.html',
@@ -6,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablesComponent implements OnInit {
 
-  constructor() { }
+  id: string | null = '';
+
+  meta: string | null = '';
+  name: string | null = '';
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // this.id = this.route.snapshot.paramMap.get('id');
+    this.meta = this.route.snapshot.paramMap.get('meta');
+
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      // this.meta = params.get('meta');
+    });
+
+    // this.name = this.route.snapshot.queryParamMap.get('name');
+
+    this.route.queryParamMap.subscribe(params => {
+      this.name = params.get('name');
+    });
+
   }
 
 }
