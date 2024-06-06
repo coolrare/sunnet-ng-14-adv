70a5645f4f8eef51700a2ceb2fe675982bfe5244
Will <doggy.huang@gmail.com>
2024-06-06 13:53:22 +0800

    示範不同的路由定義

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { TablesComponent } from './layout/tables/tables.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
+      {
+        path: '', redirectTo: 'dashboard', pathMatch: 'full'
+      },
       {
         path: 'dashboard',
         component: DashboardComponent
@@ -18,11 +22,16 @@ const routes: Routes = [
         component: TablesComponent
       }
     ]
+  },
+  {
+    path: '**', component: NotFoundComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { TablesComponent } from './layout/tables/tables.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     DashboardComponent,
-    TablesComponent
+    TablesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
