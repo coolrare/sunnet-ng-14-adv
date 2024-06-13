938774f355e7a5e3cf8dd5e8741ebcecd7cd5855
Will <doggy.huang@gmail.com>
2024-06-13 17:06:59 +0800

    修正 Bootstrap v4 的錯誤驗證的 class 使用方式

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,29 +1,46 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login2.component.html',
-  styleUrls: ['./login2.component.css']
+  styleUrls: ['./login2.component.css'],
 })
 export class Login2Component implements OnInit {
-
   data: any = {
     email: 'user@example.com',
     password: '',
-    isRememberMe: true
-  }
-
-  form: FormGroup = this.fb.group({
-    email: this.fb.control(this.data.email),
-    password: this.fb.control(this.data.password),
-    isRememberMe: this.fb.control(this.data.isRememberMe)
+    isRememberMe: true,
+  };
+  form = this.fb.group({
+    email: this.fb.control('', {
+      validators: [Validators.required, Validators.email],
+      asyncValidators: [],
+      updateOn: 'blur',
+    }),
+    password: this.fb.control('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/),
+      ],
+      asyncValidators: [],
+      updateOn: 'blur',
+    }),
+    isRememberMe: this.fb.control(true, {
+      validators: [],
+      asyncValidators: [],
+      updateOn: 'change',
+    }),
   });
-
-  constructor(@Inject(DOCUMENT) private document: Document, private fb: FormBuilder) { }
-
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private fb: FormBuilder
+  ) {}
   ngOnInit(): void {
     this.document.body.classList.add('bg-gradient-primary');
+
+    this.form.setValue(this.data);
 
     // if (this.form.invalid) {
     //   ShowError();
@@ -32,7 +49,6 @@ export class Login2Component implements OnInit {
     // if (this.form.valid) {
     //   SubmitEvent();
     // }
-
   }
   ngOnDestroy(): void {
     this.document.body.classList.remove('bg-gradient-primary');
@@ -47,5 +63,4 @@ export class Login2Component implements OnInit {
       // Show error
     }
   }
-
 }
