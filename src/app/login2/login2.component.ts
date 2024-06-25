4b028675b105fb12e62fe5b9a07748720b9f96b2
Will <doggy.huang@gmail.com>
2024-06-25 11:59:34 +0800

    示範動態控制表單欄位的作法

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT, CommonModule } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login2.component.html',
@@ -10,7 +10,7 @@ export class Login2Component implements OnInit {
   data: any = {
     users: [
       {
-        email: 'user@example.com',
+        email: 'user1@example.com',
         password: '',
       },
       {
@@ -23,7 +23,7 @@ export class Login2Component implements OnInit {
   form = this.fb.group({
     users: this.fb.array([
       this.fb.group({
-        email: this.fb.control('user2@example.com', {
+        email: this.fb.control('', {
           validators: [Validators.required, Validators.email],
           asyncValidators: [],
           updateOn: 'blur',
@@ -39,25 +39,7 @@ export class Login2Component implements OnInit {
           updateOn: 'blur',
           nonNullable: true,
         }),
-      }),
-      this.fb.group({
-        email: this.fb.control('user2@example.com', {
-          validators: [Validators.required, Validators.email],
-          asyncValidators: [],
-          updateOn: 'blur',
-          nonNullable: true,
-        }),
-        password: this.fb.control('', {
-          validators: [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/),
-          ],
-          asyncValidators: [],
-          updateOn: 'blur',
-          nonNullable: true,
-        }),
-      }),
+      })
     ]),
     isRememberMe: this.fb.control(true, {
       validators: [],
@@ -66,10 +48,34 @@ export class Login2Component implements OnInit {
       nonNullable: true,
     }),
   });
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private fb: FormBuilder
   ) {}
+
+  addUserFields(defaultEmail: string = '', defaultPassword: string = '') {
+    this.form.controls.users.push(
+      this.fb.group({
+        email: this.fb.control(defaultEmail, {
+          validators: [Validators.required, Validators.email],
+          asyncValidators: [],
+          updateOn: 'blur',
+          nonNullable: true,
+        }),
+        password: this.fb.control(defaultPassword, {
+          validators: [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/),
+          ],
+          asyncValidators: [],
+          updateOn: 'blur',
+          nonNullable: true,
+        }),
+      })
+    );
+  }
 
   doReset() {
     this.form.reset();
@@ -78,7 +84,23 @@ export class Login2Component implements OnInit {
   ngOnInit(): void {
     this.document.body.classList.add('bg-gradient-primary');
 
+    if (this.data.users.length > 0) {
+      this.form.controls.users.clear();
+      this.data.users.forEach((user: any) => {
+        this.addUserFields(user.email, user.password);
+      });
+    }
+
     this.form.setValue(this.data);
+
+    // this.form.controls.users.clear();
+    // this.form.controls.users.push();
+    // this.form.controls.users.removeAt();
+
+    // this.form.addControl();
+    // this.form.removeControl();
+
+
 
     // if (this.form.invalid) {
     //   ShowError();
